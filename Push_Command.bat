386067717d9c967a8337fd7acbe0a13b98a93de7
CIT142F-42@m307.uc-bcf.edu.ph <CIT142F-42@m307.uc-bcf.edu.ph>
2022-09-14 16:45:37 +0800

    Batch upload

--- Push_Command.bat
+++ Push_Command.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/S-Pill/CIT142F_Earl.git
+git remote set-url origin https://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@github.com/S-Pill/CIT142F_Earl.git
 git add .
 git commit -m "Batch upload"
 git push
